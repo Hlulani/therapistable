53b91b7c34a80c6e9eca665d91e9b2eed49eb529
codebender <lionelmbankeu@gmail.com>
2021-04-16 22:35:24 +0200

    new fitst commit

---
+++ components/index.js
@@ -0,0 +1,5 @@
+import { useEffect, useState,React } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link'
+
+export {Link,useRouter,useEffect,useState,React}
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import * as Component from 'components';
+
+
+export default function Index() {
+
+    return (
+
+
+        <div className="bg-black w-screen h-screen flex p-4 text-white justify-end items-end">
+        
+        </div>
+
+    )
+}
