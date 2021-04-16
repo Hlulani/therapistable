3d034654dbec997e5903bc5c34bb9d2ba2c156b5
Hlulani Baloyi <hlullyr@gmail.com>
2021-04-16 23:23:37 +0200

    added material ui

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import * as Component from 'components';
 
-
 export default function Index() {
 
     return (
 
-
         <div className="bg-black w-screen h-screen flex p-4 text-white justify-end items-end">
-        
+
         </div>
 
     )
